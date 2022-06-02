541f34750bff453f5029701009820b761e158589
Myse1f <602149025@qq.com>
2022-06-02 17:32:12 +0800

    test

--- src/core/segment_id.rs
+++ src/core/segment_id.rs
@@ -2,12 +2,10 @@ use std::cmp::{Ord, Ordering};
 use std::fmt;
 use uuid::Uuid;
 
-#[cfg(test)]
 use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
 use std::error::Error;
 use std::str::FromStr;
-#[cfg(test)]
 use std::sync::atomic;
 
 /// Uuid identifying a segment.
@@ -21,10 +19,10 @@ use std::sync::atomic;
 #[derive(Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct SegmentId(Uuid);
 
-#[cfg(test)]
+// #[cfg(test)]
 static AUTO_INC_COUNTER: Lazy<atomic::AtomicUsize> = Lazy::new(|| atomic::AtomicUsize::default());
 
-#[cfg(test)]
+// #[cfg(test)]
 const ZERO_ARRAY: [u8; 8] = [0u8; 8];
 
 // During tests, we generate the segment id in a autoincrement manner
@@ -32,16 +30,16 @@ const ZERO_ARRAY: [u8; 8] = [0u8; 8];
 //
 // The order of the test execution is not guaranteed, but the order
 // of segments within a single test is guaranteed.
-#[cfg(test)]
+// #[cfg(test)]
 fn create_uuid() -> Uuid {
     let new_auto_inc_id = (*AUTO_INC_COUNTER).fetch_add(1, atomic::Ordering::SeqCst);
     Uuid::from_fields(new_auto_inc_id as u32, 0, 0, &ZERO_ARRAY).unwrap()
 }
 
-#[cfg(not(test))]
-fn create_uuid() -> Uuid {
-    Uuid::new_v4()
-}
+// #[cfg(not(test))]
+// fn create_uuid() -> Uuid {
+//     Uuid::new_v4()
+// }
 
 impl SegmentId {
     #[doc(hidden)]
